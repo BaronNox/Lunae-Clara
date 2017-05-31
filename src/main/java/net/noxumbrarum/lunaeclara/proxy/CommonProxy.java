a11552215c1f7acae0311d26927ddde9d7cf51ef
BaronNox <baron.von.stabossen@gmail.com>
2017-05-31 19:52:12 +0200

    + CommonProxy/ServerProxy

--- src/main/java/net/noxumbrarum/lunaeclara/proxy/CommonProxy.java
+++ src/main/java/net/noxumbrarum/lunaeclara/proxy/CommonProxy.java
@@ -1,6 +1,28 @@
 package net.noxumbrarum.lunaeclara.proxy;
 
-public class CommonProxy
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.noxumbrarum.lunaeclara.hub.registries.ModBlocks;
+
+public class CommonProxy implements IProxyInits
 {
 
+	@Override
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		ModBlocks.init();
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event)
+	{
+		
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event)
+	{
+		
+	}
 }

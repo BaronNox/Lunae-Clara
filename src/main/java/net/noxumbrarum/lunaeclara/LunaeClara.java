0fea3894d098556a59ca18572c37da571c462cce
BaronNox <baron.von.stabossen@gmail.com>
2017-05-31 23:18:36 +0200

    init calls to proxy

--- src/main/java/net/noxumbrarum/lunaeclara/LunaeClara.java
+++ src/main/java/net/noxumbrarum/lunaeclara/LunaeClara.java
@@ -20,16 +20,16 @@ public class LunaeClara
 	
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
-		
+		proxy.preInit(event);
 	}
 	
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
-		
+		proxy.init(event);
 	}
 	
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
-		
+		proxy.postInit(event);
 	}
 }

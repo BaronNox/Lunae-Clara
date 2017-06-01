3ac03401645380ee7629bcee7b73062a706402c0
BaronNox <baron.von.stabossen@gmail.com>
2017-06-01 04:04:57 +0200

    testing Block Builder

--- src/main/java/net/noxumbrarum/lunaeclara/hub/registries/ModBlocks.java
+++ src/main/java/net/noxumbrarum/lunaeclara/hub/registries/ModBlocks.java
@@ -1,12 +1,16 @@
 package net.noxumbrarum.lunaeclara.hub.registries;
 
+import net.minecraft.block.material.Material;
+import net.noxumbrarum.lunaeclara.blocks.BlockGeneric;
 import net.noxumbrarum.lunaeclara.blocks.FirstBlock;
 
 public class ModBlocks
 {
-	public static FirstBlock firstBlock;
+//	public static FirstBlock firstBlock;
+	public static BlockGeneric secondBlock;
 	
 	public static void init() {
-		firstBlock = new FirstBlock();
+//		firstBlock = new FirstBlock();
+		secondBlock = new BlockGeneric.Builder("first_block", Material.IRON).build();
 	}
 }

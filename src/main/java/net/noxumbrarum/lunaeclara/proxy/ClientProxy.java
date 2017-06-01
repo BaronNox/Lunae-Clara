6fbc4ca586a8cd191a3a659267b5245a49a05797
BaronNox <baron.von.stabossen@gmail.com>
2017-06-01 04:05:18 +0200

    + init itemblock registry

--- src/main/java/net/noxumbrarum/lunaeclara/proxy/ClientProxy.java
+++ src/main/java/net/noxumbrarum/lunaeclara/proxy/ClientProxy.java
@@ -1,6 +1,14 @@
 package net.noxumbrarum.lunaeclara.proxy;
 
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.noxumbrarum.lunaeclara.hub.registries.ModBlocks;
+
 public class ClientProxy extends CommonProxy
 {
-
+	@Override
+	public void preInit(FMLPreInitializationEvent event)
+	{
+		super.preInit(event);
+		ModBlocks.secondBlock.initModel();
+	}
 }

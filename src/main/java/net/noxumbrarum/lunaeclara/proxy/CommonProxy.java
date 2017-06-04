196089791636448d2ebd4b066bf1cf74d6c495ed
BaronNox <baron.von.stabossem@gmail.com>
2017-06-04 03:54:29 +0200

    +creative tab for mod blocks

--- src/main/java/net/noxumbrarum/lunaeclara/proxy/CommonProxy.java
+++ src/main/java/net/noxumbrarum/lunaeclara/proxy/CommonProxy.java
@@ -4,13 +4,17 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.noxumbrarum.lunaeclara.hub.registries.ModBlocks;
+import net.noxumbrarum.lunaeclara.misc.creativetabs.CreativeTabBlocks;
 
 public class CommonProxy implements IProxyInits
 {
+	private CreativeTabBlocks creativeTabModBlocks;
 
 	@Override
 	public void preInit(FMLPreInitializationEvent event)
 	{
+		creativeTabModBlocks = CreativeTabBlocks.getInstance();
+		
 		ModBlocks.init();
 	}
 

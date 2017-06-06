b6c0d6b3a12268bfd1cd925abfb75611ffc75970
BaronNox <baron.von.stabossem@gmail.com>
2017-06-07 01:57:02 +0200

    -debug

--- src/main/java/net/noxumbrarum/lunaeclara/client/gui/screens/BaseScreen.java
+++ src/main/java/net/noxumbrarum/lunaeclara/client/gui/screens/BaseScreen.java
@@ -3,11 +3,29 @@ package net.noxumbrarum.lunaeclara.client.gui.screens;
 import net.minecraft.client.gui.GuiScreen;
 
 public class BaseScreen extends GuiScreen{
+	private boolean pauseGame;
+	
+	public BaseScreen() {
+		this(false);
+	}
+	
+	public BaseScreen(boolean pauseGame) {
+		super();
+		this.pauseGame = pauseGame;
+	}
 	
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 		this.drawDefaultBackground();
-		System.out.println("DRAWN");
-		super.drawScreen(mouseX, mouseY, partialTicks);
+//		super.drawScreen(mouseX, mouseY, partialTicks);
+	}
+	
+	@Override
+	public boolean doesGuiPauseGame() {
+		return pauseGame;
+	}
+	
+	public void setPauseGame(boolean value) {
+		pauseGame = value;
 	}
 }
